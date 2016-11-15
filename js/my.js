97686fccf1240134f2954d617efbaa292c2db55e
Surface Pro 4\Nicki <nicki_1992@hotmail.com>
2016-11-15 18:47:41 +0100

    Done

--- js/my.js
+++ js/my.js
@@ -6,9 +6,9 @@
 var user;
 var balance;
 var currency;
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $.get("http://52.57.228.6//man2API/php/BankPhp.php?what=account_info&apikey=7e615a98c167b5592497d27b4f1d80af", function(data){
+    $.get("http://52.57.228.6//man2API/php/BankPhp.php?what=account_info&apikey=7e615a98c167b5592497d27b4f1d80af", function (data) {
         var accInfo = $.parseJSON(data);
         console.log(accInfo);
 
@@ -19,21 +19,26 @@ $(document).ready(function(){
         $("#name").append(user);
         $("#balance").append(balance);
         $("#currency").append(currency);
-        updateOffers();
+
+        //Updates all data
+        update();
+        // Starts updateTimer
+        updateTimer();
     });
 });
 
 // Make Offer - Label + button to make sale
-function makeOffer(){
+function makeOffer() {
     var amount = $("#myOffer").val();
-    $.get("http://52.57.228.6//man2API/php/BankPhp.php?what=sell&amount=" + amount + "&apikey=7e615a98c167b5592497d27b4f1d80af", function(data){
+    $.get("http://52.57.228.6//man2API/php/BankPhp.php?what=sell&amount=" + amount + "&apikey=7e615a98c167b5592497d27b4f1d80af", function (data) {
         update();
+        $("#myOffer").val("");
     });
 };
 
 function buy(id) {
 
-    $.get("http://52.57.228.6//man2API/php/BankPhp.php?what=buy&offer=" + id + "&apikey=7e615a98c167b5592497d27b4f1d80af", function(data) {
+    $.get("http://52.57.228.6//man2API/php/BankPhp.php?what=buy&offer=" + id + "&apikey=7e615a98c167b5592497d27b4f1d80af", function (data) {
         console.log("Success");
         update();
     });
@@ -41,9 +46,10 @@ function buy(id) {
 
 function exchange() {
     var otherCurrency = $("#myExchange").val();
-    $.get("http://52.57.228.6//man2API/php/BankPhp.php?what=exchange_rate&from="+ otherCurrency +
-        "&to=" + currency + "&apikey=7e615a98c167b5592497d27b4f1d80af", function(data){
-        var exchangeInfo = $.parseJSON(data);;
+    $.get("http://52.57.228.6//man2API/php/BankPhp.php?what=exchange_rate&from=" + otherCurrency +
+        "&to=" + currency + "&apikey=7e615a98c167b5592497d27b4f1d80af", function (data) {
+        var exchangeInfo = $.parseJSON(data);
+        ;
         var exchangeRate = exchangeInfo.data.amount;
 
         $("#exRate").empty();
@@ -65,31 +71,36 @@ function updateOffers() {
             var id = offer.id;
             var date = offer.since;
 
-
             $("#offerBody").append(
                 "<tr>" +
-                    "<td>" + currency + "</td>" +
-                    "<td>" + amount + "</td>" +
-                    "<td>" + id + "</td>" +
-                    "<td>" + date + "</td>" +
-                    "<td> <button class='btn btn-default' onclick='buy(" + id + ");'>Buy</button> </td>" +
+                "<td>" + currency + "</td>" +
+                "<td>" + amount + "</td>" +
+                "<td>" + id + "</td>" +
+                "<td>" + date + "</td>" +
+                "<td> <button class='btn btn-default' onclick='buy(" + id + ");'>Buy</button> </td>" +
                 "</tr>"
             );
         }
     });
 }
 
+function updateData() {
+    $.get("http://52.57.228.6//man2API/php/BankPhp.php?what=account_info&apikey=7e615a98c167b5592497d27b4f1d80af", function (data) {
+        var accInfo = $.parseJSON(data);
+        $("#balance").empty();
+        $("#balance").append("Balance: " + accInfo.data[0].amount);
+    });
+}
+
+function update() {
+    updateData();
+    updateOffers();
+}
+
 //Updates data
-function update(){
-    setInterval(function(){
-        $.get("http://52.57.228.6//man2API/php/BankPhp.php?what=account_info&apikey=7e615a98c167b5592497d27b4f1d80af", function(data){
-            var accInfo = $.parseJSON(data);
-            console.log(accInfo);
-
-            $("#balance").empty();
-            $("#balance").append("Balance: " + accInfo.data[0].amount);
-
-            updateOffers();
-        });
-    }, 1000);
+function updateTimer() {
+    setInterval(function () {
+        updateData();
+        updateOffers();
+    }, 5000);
 }
